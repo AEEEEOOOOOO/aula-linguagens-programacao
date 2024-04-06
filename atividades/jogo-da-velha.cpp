38510c672392e46066e23fd1699da77460c772b1
Caio Riquelmy <riqu3lmy@gmail.com>
2024-04-05 21:13:47 -0400

    Idade e altura em C++

---
+++ atividades/jogo-da-velha.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float altura;
+    int idade;
+
+    cout << "Digite a sua idade: ";
+    cin >> idade;
+    cout << "Digite a sua altura: ";
+    cin >> altura;
+
+    cout << "A sua altura eh: " << altura << endl;
+    cout << "A sua idade eh: " << idade << endl;
+
+    return 0;
+}
